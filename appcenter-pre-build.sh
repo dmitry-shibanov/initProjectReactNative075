458314fe12897c86a0eda7dd8e1658485d7bea05
Dmitry Shibanov <shibanov-1997@inbox.ru>
2024-09-17 15:11:45 +0200

    add creation of directory

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 echo "prebuild script started"
 ls
+mkdir ./node_modules/react-native/local-cli
 echo "require('@react-native-community/cli').run();" >> ./node_modules/react-native/local-cli/cli.js
